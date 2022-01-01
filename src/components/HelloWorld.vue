93b93580f03f2764d8fcd7e7236bb3d6a79fd274
ema.ferr <emanueleferrauto@outlook.it>
2022-01-01 22:42:29 +0100

    Start

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>Test</h1>
+    <h2>Family</h2>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  .hello{
+    h1{
+      color: red;
+    }
+  }
+
+</style>
