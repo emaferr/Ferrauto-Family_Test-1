1bd103c4e65232315cfb0ecca7fee29b44a00a3d
ema.ferr <emanueleferrauto@outlook.it>
2022-01-01 22:54:16 +0100

    Install axios with Vue ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="hello">
-    <h1>Test</h1>
-    <h2>Family</h2>
+    <h1>Family</h1>
   </div>
 </template>
 
 <script lang="ts">
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
-  .hello{
-    h1{
-      color: red;
-    }
-  }
-
 </style>
